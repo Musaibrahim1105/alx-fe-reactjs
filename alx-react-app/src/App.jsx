94cab32c7a4b260a23455b15b5e805639e2a171e
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-02-14 13:52:38 +0100

    done

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header.jsx'
-import MainContent  from './components/MainContent.jsx'
+import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
 
