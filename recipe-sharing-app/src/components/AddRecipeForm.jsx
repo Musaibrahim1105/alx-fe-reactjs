7e6179f89ced09e1e7160dbaab9b832e0d2b9e84
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-03-22 22:43:54 +0100

    task 0 done

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,7 +1,7 @@
 
  // AddRecipeForm component
  import { useState } from 'react';
- import { useRecipeStore } from './recipeStore';
+ import { useRecipeStore } from '/recipeStore';
 
  const AddRecipeForm = () => {
    const addRecipe = useRecipeStore(state => state.addRecipe);
